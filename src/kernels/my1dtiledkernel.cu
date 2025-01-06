471051454c27de0f7b4abd5af7d38f79c2916a7e
Raghav Kumar <thisisraghavkumar@gmail.com>
2025-01-05 20:16:04 -0500

    Added 1d tiled kernel and renamed shared memory kernel file name

---
+++ src/kernels/my1dtiledkernel.cu
@@ -0,0 +1,48 @@
+#include "mykernels.cuh"
+#include "../helpers/myhelpers.h"
+
+template <const int BM, const int BK, const int BN, const int TM> 
+__global__ void my1dtiledkernel(float *A, float *B, float *C, int m, int k, int n){
+    int frameRow = blockIdx.y;
+    int frameCol = blockIdx.x;
+
+    __shared__ float As[BM*BK];
+    __shared__ float Bs[BK*BN];
+
+    int threadRow = threadIdx.x / BN;
+    int threadCol = threadIdx.x % BN;
+    int ARow = threadIdx.x / BK;
+    int ACol = threadIdx.x % BK;
+    int BRow = threadIdx.x / BN;
+    int BCol = threadIdx.x % BN;
+
+    A += (frameRow * BM * k);
+    B += (frameCol * BN);
+    C += (frameRow * BM * n) + (frameCol * BN);
+
+    float results[TM] = {0.0f};
+    for(int idx=0;idx<k;idx+=BK){
+        As[ARow * BK + ACol] = A[ARow * k + ACol];
+        Bs[BRow * BN + BCol] = B[BRow * n + BCol];
+        __syncthreads();
+
+        A += BK;
+        B += (BK * N);
+        for(int l=0; l<BK; l++){
+            float temp = Bs[l * BN + threadCol];
+            for(int i=0; i<TM; i++){
+                results[i] += As[(threadRow * TM + i) * BK + l] * temp;
+            }
+        }
+        __syncthreads();
+    }
+    for(int i=0; i<TM;i++){
+        C[(threadRow*TM+i)*BN + threadCol] = results[i];
+    }
+}
+
+template <const int BM, const int BK, const int BN, const int TM> void invoke_1D_tiled_matmul(float *A, float *B, float *C, int m, int k, int n){
+    dim3 gridDimension(CEILDIV(N, BN), CEILDIV(M, BM));
+    dim3 blockDimension((BN * BM)/TM);
+    my1dtiledkernel<BM,BK,BN,TM><<<gridDimension,blockDimension>>>(A, B, C, m, k, n);
+}
--- src/program.cu
+++ src/program.cu
@@ -98,6 +98,7 @@ int main(){
     float naive_time = run_kernel("naive", invoke_naive_matmul, d_A, d_B, d_C, m, k, n, h_C, h_C_cublas, gen, warmup_runs, measurement_runs);
     float row_coalesce_time = run_kernel("row_coalesce", invoke_rowmajor_matmul, d_A, d_B, d_C, m, k, n, h_C, h_C_cublas, gen, warmup_runs, measurement_runs);
     float shared_memory_time = run_kernel("shared_memory", invoke_shared_memory_matmul, d_A, d_B, d_C, m, k, n, h_C, h_C_cublas, gen, warmup_runs, measurement_runs);
+    float oned_tiled_time = run_kernel("1_d_tiled", invoke_1D_tiled_matmul<64,8,64,8>, d_A, d_B, d_C, m, k, n, h_C, h_C_cublas, gen, warmup_runs, measurement_runs);
 
     cudaEventRecord(cublasBeg);
     for(int i=0; i<measurement_runs; i++){
@@ -119,6 +120,8 @@ int main(){
     std::cout<<"Row coalesce Kernel GFLOPS: "<<(numoperations / ((row_coalesce_time/measurement_runs) / 1000)) / 1e9<<std::endl;
     std::cout<<"Time taken by shared memory kernel: "<<shared_memory_time/measurement_runs<<" ms"<<std::endl;
     std::cout<<"Shared Memory Kernel GFLOPS: "<<(numoperations / ((shared_memory_time/measurement_runs) / 1000)) / 1e9<<std::endl;
+    std::cout<<"Time taken by 1-d tiled kernel: "<<oned_tiled_time/measurement_runs<<" ms"<<std::endl;
+    std::cout<<"1D Tiled Kernel GFLOPS: "<<(numoperations / ((oned_tiled_time/measurement_runs) / 1000)) / 1e9<<std::endl;
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
