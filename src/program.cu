b621c1a0ea4a56ef56d6a99e592cdc6e5e51cf79
Raghav Kumar <thisisraghavkumar@gmail.com>
2025-01-11 01:32:05 -0500

    Moved kernel 2 and 1 code to program.cu

--- src/program.cu
+++ src/program.cu
@@ -18,6 +18,47 @@
         }                                                                \
     } while (0)
 
+//Kernel 1
+__global__ void mynaivekernel(float *A, float *B, float *C, int m, int k, int n){
+    int firstrow = blockIdx.y * blockDim.y + threadIdx.y;
+    int secondcol = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if(firstrow < m && secondcol < n){
+        float sum = 0.0f;
+        for(int i=0; i<k; i++){
+            sum += A[firstrow * k + i] * B[i*n + secondcol];
+        }
+        C[firstrow * n + secondcol] = sum;
+    }
+}
+
+void invoke_naive_matmul(float *A, float *B, float *C, int m, int k, int n){
+    dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 gridSize(CEILDIV(n, BLOCK_SIZE), CEILDIV(m, BLOCK_SIZE));
+
+    mynaivekernel<<<gridSize, blockSize>>>(A, B, C, m, k, n);
+}
+
+__global__ void myRowCoalesceKernel(float *A, float *B, float *C, int m, int k, int n){
+    int firstRow = blockIdx.x * BLOCK_SIZE + threadIdx.x/blockDim.x;
+    int secondCol = blockIdx.y * BLOCK_SIZE + threadIdx.x%blockDim.x;
+
+    if(firstRow < m && secondCol < n){
+        float sum = 0.0f;
+        for(int i=0; i<k; i++){
+            sum += A[firstRow * k + i] * B[i * n + secondCol];
+        }
+        C[firstRow * n + secondCol] = sum;
+    }
+}
+
+void invoke_rowmajor_matmul(float *A, float *B, float *C, int m, int k, int n){
+    dim3 blockSize(BLOCK_SIZE*BLOCK_SIZE);
+    dim3 gridSize(CEILDIV(m, BLOCK_SIZE), CEILDIV(n, BLOCK_SIZE));
+
+    myRowCoalesceKernel<<<gridSize, blockSize>>>(A, B, C, m, k, n);
+}
+
 /*
  * Function to populate an array of floats with random values
  */
