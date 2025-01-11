fd65694871ce9a171e0fb86e9d02763f812555bb
Raghav Kumar <thisisraghavkumar@gmail.com>
2025-01-10 22:10:35 -0500

    Made kernel runner function more dynamic by using tempalte

--- src/kernels/kernelrunner.cu
+++ src/kernels/kernelrunner.cu
@@ -9,7 +9,8 @@
 /**
  * Returns the time taken by invoking the passed kernel on matrices A and B measurement_iterations times.
  */
-float run_kernel(const char* kernel_name, void (*invoke_kernel)(float *, float *, float *, int, int, int), float *d_A, float *d_B, float *d_C, int m, int k, int n, float *h_C, float *h_C_ref, std::mt19937 gen, int warmup_runs, int measurement_runs){
+template <typename KernelFunc> 
+float run_kernel(const char* kernel_name, KernelFunc invoke_kernel, float *d_A, float *d_B, float *d_C, int m, int k, int n, float *h_C, float *h_C_ref, std::mt19937 gen, int warmup_runs, int measurement_runs){
     cudaEvent_t beg, end;
     float elapsed_time;
     cudaEventCreate(&beg);
